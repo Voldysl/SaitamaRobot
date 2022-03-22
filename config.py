b79b1eb6844efc605b4251ae36051e5f779ab568
Voldysl <87264852+Voldysl@users.noreply.github.com>
2022-03-22 13:36:54 -0700

    Update config.py

--- config.py
+++ config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 792109647  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "Sawada"
-    SUPPORT_CHAT = "OnePunchSupport"  # Your own group for support, do not add the @
+    API_ID = 3135143  # integer value, dont use ""
+    API_HASH = "24f97a7491f6fc888eeff31694c061bf"
+    TOKEN = "5293422251:AAF3rtA46sJiWV6P-1l-6pOIuGItFN9IiY4"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1234523543 # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "iron_voldy"
+    SUPPORT_CHAT = "hagridbot"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001253661229
+        -1001593709176
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001190806654
+        -1001593709176
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
